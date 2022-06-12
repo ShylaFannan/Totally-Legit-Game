dcd1c8e4d2dec1fe20afc98ca873b27d82b20ead
Shyla Fannan <sokawaiiidie@gmail.com>
2022-06-12 15:19:03 -0500

    created laser behavior

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
   private float _speed = 3.5f;
   [SerializeField]
   private GameObject _laserPrefab;
+  [SerializeField]
+  private float _fireRate = 0.25f;
+  private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,9 +23,10 @@ public class Player : MonoBehaviour
     {
      CalculateMovement(); 
      //if i hit the space key spawn gameObject
-   if (Input.GetKeyDown(KeyCode.Space))
+   if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
       {
-        Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
       }
  
     }

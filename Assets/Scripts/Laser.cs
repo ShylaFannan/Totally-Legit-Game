a8445468e396dffb2dc9fc6390da1ed306a4880c
Shyla Fannan <sokawaiiidie@gmail.com>
2022-06-12 20:41:09 -0500

    added enemy

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -6,20 +6,11 @@ public class Laser : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 8.0f; 
-    //speed variable of 8
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+ 
     void Update()
     {
-        //translate laser up
         transform.Translate (Vector3.up * _speed * Time.deltaTime);
-        //is laser position is greater than 8 on the y
-        //destroy the object
+        
          if (transform.position.y > 8f)
          {
             Destroy(this.gameObject);
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,23 +12,19 @@ public class Player : MonoBehaviour
   private float _fireRate = 0.25f;
   private float _canFire = -1f;
 
-    // Start is called before the first frame update
     void Start()
     {
       transform.position = new Vector3 (0, 0, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
      CalculateMovement(); 
-     //if i hit the space key spawn gameObject
-   if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
-      {
-        _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
-      }
- 
+
+     if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+     {
+      FireLaser();
+     }
     }
     void CalculateMovement()
     {
@@ -53,4 +49,9 @@ public class Player : MonoBehaviour
         transform.position = new Vector3(11.3f, transform.position.y, 0);
       }
     }
+void FireLaser()
+      {
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+      }
 }

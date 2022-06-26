4a49b851997aa641c64b6a7618fc83b821085d2b
Shyla Fannan <sokawaiiidie@gmail.com>
2022-06-26 15:12:44 -0500

    added restart after death

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,11 +14,22 @@ public class UIManager : MonoBehaviour
     private Image _LivesImg;
     [SerializeField]
     private Sprite[] _liveSprites;
+    [SerializeField]
+    private TMP_Text _restartText;
+    [SerializeField]
+    private GameManager _gameManager;
   
     void Start()
     {
         _scoreText.text = "Score:" + 0;
         _gameOverText.gameObject.SetActive(false);
+        _restartText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+
+        if(_gameManager == null)
+        {
+            Debug.LogError("GameManager is Null");
+        }
     }
 
     public void UpdateScore(int playerScore)
@@ -28,13 +39,19 @@ public class UIManager : MonoBehaviour
 
     public void UpdateLives(int currentLives)
     {
-
         _LivesImg.sprite = _liveSprites[currentLives];
         //display img sprite = current amount of lives 0-3
 
         if (currentLives == 0)
         {
-            _gameOverText.gameObject.SetActive(true);
+            GameOverSequence();
         }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
     }
 }

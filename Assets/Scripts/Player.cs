07247feb7a51545e2e0ac81130fcde66484e0736
Shyla Fannan <sokawaiiidie@gmail.com>
2022-06-27 23:18:42 -0500

    broke and fixed asteroid, created engine failures

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,107 +19,115 @@ public class Player : MonoBehaviour
   private int _lives = 3;
   private SpawnManager _spawnManager;
   private bool _isTripleShotActive = false;
-  //private bool _isSpeedBoostActive = false;
   private bool _isShieldActive = false;
   [SerializeField]
   private GameObject _shieldVisualizer;
   [SerializeField]
   private int _score;
   private UIManager _uiManager;
+  [SerializeField]
+  private GameObject _rightEngine, _leftEngine; //this
 
-    void Start()
+  void Start()
+  {
+    transform.position = new Vector3 (0, 0, 0);
+    _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+    _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+
+    if (_spawnManager == null)
     {
-      transform.position = new Vector3 (0, 0, 0);
-     _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
-      _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
-      if (_spawnManager == null)
-      {
-        Debug.LogError("The Spawn Manager is Null");
-      }
-
-      if(_uiManager == null)
-      {
-          Debug.LogError("The UI Manager is NULL.");
-      }
-
+      Debug.LogError("The Spawn Manager is Null");
     }
+
+    if(_uiManager == null)
+    {
+      Debug.LogError("The UI Manager is NULL.");
+    }
+  }
       
 
 
-    void Update()
-    {
-     CalculateMovement(); 
+  void Update()
+  {
+    CalculateMovement(); 
 
-     if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
-     {
+    if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+    {
       FireLaser();
-     }
-
     }
-    void CalculateMovement()
+  }
+
+  void CalculateMovement()
+  {
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input. GetAxis("Vertical");
+    Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+    transform.Translate (direction * _speed * Time.deltaTime);
+
+    if (transform.position.y>=0)
     {
-      float horizontalInput = Input.GetAxis("Horizontal");
-      float verticalInput = Input. GetAxis("Vertical");
-      Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
-      transform.Translate (direction * _speed * Time.deltaTime);
-
-      if (transform.position.y>=0)
-      {
-        transform.position = new Vector3(transform.position.x,0,0);
-      }
-
-      else if (transform.position.y<=-3.8f)
-      {
-        transform.position = new Vector3(transform.position.x,0,0);
-      }
-
-      if (transform.position.x>11.3f)
-      {
-        transform.position = new Vector3(-11.3f, transform.position.y, 0);
-      }
-
-      else if (transform.position.x <-11.3f)
-      {
-        transform.position = new Vector3(11.3f, transform.position.y, 0);
-      }
+      transform.position = new Vector3(transform.position.x,0,0);
     }
 
-      
-    
-void FireLaser()
-      {
-        _canFire = Time.time + _fireRate;
+    else if (transform.position.y<=-3.8f)
+    {
+      transform.position = new Vector3(transform.position.x,0,0);
+    }
 
-        if (_isTripleShotActive == true)
-        {
-          Instantiate(_tripleShotPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
-        }
+    if (transform.position.x>11.3f)
+    {
+      transform.position = new Vector3(-11.3f, transform.position.y, 0);
+    }
 
-        else
-        {
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
-        }
+    else if (transform.position.x <-11.3f)
+    {
+      transform.position = new Vector3(11.3f, transform.position.y, 0);
+    }
+  }
 
-      }
+  void FireLaser()
+  {
+    _canFire = Time.time + _fireRate;
 
-public void Damage()
+    if (_isTripleShotActive == true)
+    {
+      Instantiate(_tripleShotPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+    }
+
+    else
+    {
+      Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+    }
+  }
+
+  public void Damage()
   {
     if (_isShieldActive == true)
     {
-       _isShieldActive = false;
-       _shieldVisualizer.SetActive(false);
-       return;
+      _isShieldActive = false;
+      _shieldVisualizer.SetActive(false);
+      return;
     }
 
-     _lives--;
-     _uiManager.UpdateLives(_lives);
-   
+    _lives--;
+
+    if(_lives == 2)
+    {
+      _leftEngine.SetActive(true);
+    }
+
+    else if(_lives == 1)
+    {
+      _rightEngine.SetActive(true);
+    }
+
+    _uiManager.UpdateLives(_lives);
+
     if (_lives < 1)
     {
       _spawnManager.OnPlayerDeath();
       Destroy(this.gameObject);
     }
-
   }
 
   public void TripleShotActive()

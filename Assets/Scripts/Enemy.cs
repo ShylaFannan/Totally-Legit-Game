e9d0b005834b47a17a2e1731b3622f694c624e9a
Shyla Fannan <sokawaiiidie@gmail.com>
2022-07-02 13:30:54 -0500

    fixed bug and added enemy lasers

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,6 +6,10 @@ public class Enemy : MonoBehaviour
 {
   [SerializeField]
   private float _speed = 4.0f;
+  [SerializeField]
+  private GameObject _laserPrefab;
+  private float _fireRate = 3.0f;
+  private float _canFire = -1;
   private Player _player;
   private Animator _anim;
   private AudioSource _audioSource; 
@@ -27,40 +31,61 @@ public class Enemy : MonoBehaviour
     }
 
     _audioSource = GetComponent<AudioSource>();
+
+    if (_audioSource == null)
+    {
+      Debug.LogError("AudioSource on the player is null!");
+    }
   }
 
-    void Update()
+  void Update()
+    {
+      CalculateMovement();
+
+      if(Time.time > _canFire)
+      {
+        _fireRate = Random.Range(3f, 7f);
+        _canFire = Time.time + _fireRate;
+        GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+        
+        for (int i = 0; i < lasers.Length; i++)
+        {
+          lasers[i].AssignEnemyLaser();
+        }
+      }
+    }
+    
+    void CalculateMovement()
     {
        transform.Translate (Vector3.down * _speed * Time.deltaTime); 
 
         if (transform.position.y < -5f)
-      {
-        float randomX = Random.Range(-8f, 8f);
-        transform.position = new Vector3(randomX, 7, 0);
-      }
-
+        {
+          float randomX = Random.Range(-8f, 8f);
+          transform.position = new Vector3(randomX, 7, 0);
+        }
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
       if (other.tag == "Player")
       {
-       Player player = other.transform.GetComponent<Player>();
+        Player player = other.transform.GetComponent<Player>();
 
-       if (player != null)
-       {
+      if (player != null)
+      {
         player.Damage();
-       }
-       
-       _anim.SetTrigger("OnEnemyDeath");
-       _speed = 0;
-       _audioSource.Play();
+      }
+
+      _anim.SetTrigger("OnEnemyDeath");
+      _speed = 0;
+      _audioSource.Play();
         Destroy(this.gameObject, 2.8f);
       }
       
       if (other.tag == "laser")
       {
-        _audioSource.Play();
         Destroy(other.gameObject);
 
         if (_player != null)
@@ -71,7 +96,9 @@ public class Enemy : MonoBehaviour
         _anim.SetTrigger("OnEnemyDeath");
         _speed = 0;
         _audioSource.Play();
+        Destroy(GetComponent<Collider2D>());
         Destroy(this.gameObject, 2.8f);
+      }
     }
 }
-}
+
